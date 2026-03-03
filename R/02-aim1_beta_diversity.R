8f647197f8d112e05d63fea44f733681969e0b00
Marcus <marcusliang26@gmail.com>
2026-03-02 16:49:17 -0800

    Added suggestions from meeting with Hans

--- R/02-aim1_beta_diversity.R
+++ R/02-aim1_beta_diversity.R
@@ -36,6 +36,7 @@ group_cols <- c(
   "CPP Endo" = "lightpink"
 )
 
+####first attempt####
 gg_uni <- plot_ordination(ps_rarefied, pcoa_uni, type="samples",
                           color="Host_disease") +
   geom_point(size=3.5, alpha=0.9) +
@@ -77,3 +78,31 @@ permanova_vagina <- adonis2(dm_vagina ~ Host_disease,
                             permutations = 999)
 permanova_vagina
 
+####revised####
+# Extract % variation for axes
+eig_vals <- pcoa_uni$values$Eigenvalues
+var_explained <- round(eig_vals / sum(eig_vals) * 100, 1)
+
+# PERMANOVA p-value (overall)
+pval <- permanova_all$`Pr(>F)`[2]  # Host_disease term
+
+# Create new PCoA plot object
+gg_uni_ellipse <- plot_ordination(ps_rarefied, pcoa_uni, type = "samples", color = "Host_disease") +
+  geom_point(size = 3.5, alpha = 0.9) +
+  stat_ellipse(aes(group = Host_disease, color = Host_disease), level = 0.95, linewidth = 1) +
+  theme_bw() +
+  scale_color_manual(values = group_cols) +
+  labs(
+    color = "Host Disease",
+    x = paste0("PCoA Axis 1 (", var_explained[1], "%)"),
+    y = paste0("PCoA Axis 2 (", var_explained[2], "%)"),
+    title = "Host disease/Sample type: Unweighted UniFrac"
+  )
+
+# Display the new plot
+gg_uni_ellipse
+
+# Save new plot
+ggsave("results/aim1/02-pcoa_ellipse.png",
+       plot = gg_uni_ellipse,
+       width = 10, height = 7, units = "in", dpi = 300)
