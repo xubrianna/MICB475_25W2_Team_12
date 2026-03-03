389bb8353b0385fb928ac838593978728238e9e2
Marcus <marcusliang26@gmail.com>
2026-03-02 17:06:56 -0800

    m

--- R/02-aim1_beta_diversity.R
+++ R/02-aim1_beta_diversity.R
@@ -99,13 +99,12 @@ gg_uni_ellipse <- (
     y = paste0("PCoA Axis 2 (", var_explained[2], "%)"),
     title = "Host disease/Sample type: Unweighted UniFrac"
   )
-  + scale_x_continuous(
-    expand = expansion(mult = 0.25),
-    breaks = seq(floor(min(pcoa_uni$vectors[,1])*10)/10, ceiling(max(pcoa_uni$vectors[,1])*10)/10, by = 0.1)
-  )
+  + scale_x_continuous(expand = expansion(mult = 0.25))  
   + scale_y_continuous(
     expand = expansion(mult = 0.25),
-    breaks = seq(floor(min(pcoa_uni$vectors[,2])*10)/10, ceiling(max(pcoa_uni$vectors[,2])*10)/10, by = 0.1)
+    breaks = seq(floor(min(pcoa_uni$vectors[,2])*10)/10,
+                 ceiling(max(pcoa_uni$vectors[,2])*10)/10,
+                 by = 0.1)
   )
 )
 
