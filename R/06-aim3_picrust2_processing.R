c1314d33454af42717d92add665085031c53b0b5
Tom Cui <tomcui@MacBook-Air.local>
2026-03-05 09:53:52 -0800

    picrust update 1

--- R/06-aim3_picrust2_processing.R
+++ R/06-aim3_picrust2_processing.R
@@ -1,2 +1,17 @@
-testing 
-testing2
+install.packages("devtools")
+devtools::install_github("cafferychen777/ggpicrust2")
+install.packages("MicrobiomeStat")
+install.packages("GGally")
+install.packages("phyloseq")
+
+library(tidyverse)
+library(phyloseq)
+library(ggpicrust2)
+
+ps <- readRDS("data/phyloseq_filtered.rds")
+
+meta <- sample_data(ps) %>%
+  data.frame() %>%
+  rownames_to_column("sample_name")
+ko <- read.delim("data/pred_metagenome_unstrat.tsv")
+head(ko[,1:6])
