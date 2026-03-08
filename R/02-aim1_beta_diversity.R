a78798ba5a18537ded3e952780dcd875564d36ee
Marcus <marcusliang26@gmail.com>
2026-03-07 16:08:51 -0800

    M

--- R/02-aim1_beta_diversity.R
+++ R/02-aim1_beta_diversity.R
@@ -31,9 +31,9 @@ sample_data(ps_rarefied)$env_medium <- factor(meta_df$env_medium,
 
 
 group_cols <- c(
-  "Control"  = "steelblue1",
-  "CPP"      = "mediumpurple1",
-  "CPP Endo" = "lightpink"
+  "Control" = "#c7e9b4",   # green
+  "CPP" = "#41b6c4",       # turquoise
+  "CPP Endo" = "#225ea8"   # dark blue
 )
 
 ####first attempt####
