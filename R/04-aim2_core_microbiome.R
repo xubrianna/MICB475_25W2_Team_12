08faa7383840bbde193c47b3105c319f7a6c2ddf
Naomi Isak <naomi_isak@dhcp-206-87-236-218.ubcsecure.wireless.ubc.ca>
2026-03-16 12:50:38 -0700

    saved core microbiome vaginal prevalence 0.1

--- R/04-aim2_core_microbiome.R
+++ R/04-aim2_core_microbiome.R
@@ -4,7 +4,7 @@ library(phyloseq)
 library(microbiome)
 library(ggVennDiagram)
 
-##LOAD AND MODIFY PHYLOSEQ
+####LOAD AND MODIFY PHYLOSEQ####
 #load filtered phyloseq without rarification (expects uneven sequencing depths)
 phyloseq <- readRDS('data/phyloseq_filtered.rds')
 
@@ -15,33 +15,42 @@ CPP_RA <- transform_sample_counts(phyloseq, fun=function(x) x/sum(x))
 otu_table(phyloseq)
 sample_data(phyloseq)
 
-## STRATIFY BY BODY SITE 
-# RECTAL
+#### STRATIFY BY HOST DISEASE #### 
+
 #try stratifying by the rectal sample after stratify by disease 
 CPP_only <- subset_samples(CPP_RA, `Host_disease`=="CPP")
 CPP_endo <- subset_samples(CPP_RA, `Host_disease`=="CPP Endo")
 CPP_healthy <-subset_samples(CPP_RA, `Host_disease`=="Control")
 
+#### RECTAL COREMICROBIOME #### 
+#filter by rectal samples 
 CPP_only_rectal <- subset_samples(CPP_only,`collection_method` == "rectal_swab")
 CPP_endo_rectal <- subset_samples(CPP_endo,`collection_method` == "rectal_swab")
 CPP_healthy_rectal <-subset_samples(CPP_healthy, `collection_method` == "rectal_swab")
 
-#then assign ASVs - 0.001 filters out rare
+#assign ASVs using 0.001 detection (filters out rare taxa) and 0.5 prevalence
 CPP_only_ASVs_rectal_0.001 <- core_members(CPP_only_rectal, detection=0.001, prevalence = 0.5)
 CPP_endo_ASVs_rectal_0.001 <- core_members(CPP_endo_rectal, detection=0.001, prevalence = 0.5)
 CPP_healthy_ASVs_rectal_0.001 <- core_members(CPP_healthy_rectal, detection=0.001, prevalence = 0.5)
 
-#then make a list
 CPP_list_rectal_0.001 <- list(Healthy = CPP_healthy_ASVs_rectal_0.001, CPP_Only = CPP_only_ASVs_rectal_0.001, CPP_Endo = CPP_endo_ASVs_rectal_0.001)
 
-#then make venn diagram 
 venn_all_diseases_rectal_0.001 <- ggVennDiagram(x = CPP_list_rectal_0.001)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
   labs(title = "Rectal Samples") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   theme(plot.title = element_text(size=20, face="bold"))
 venn_all_diseases_rectal_0.001
 
-ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.001.png", venn_all_diseases_rectal_0.001)
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.001.png", venn_all_diseases_rectal_0.001, width = 7, height = 7)
+
+#get the taxonomic information for CPP_endo disease group and find the ASVs that are unique to the core of CPP-endo 
+tax_mat_rectal <- tax_table(CPP_endo_rectal)
+
+core_taxonomy_rectal <- as.data.frame(tax_mat[CPP_endo_ASVs_rectal_0.001, ])
+print(head(core_taxonomy))
+
+unique_to_CPP_endo_rectal <- setdiff(CPP_endo_ASVs_rectal_0.001, union(CPP_only_ASVs_rectal_0.001, CPP_healthy_ASVs_rectal_0.001))
+print(unique_to_CPP_endo_rectal)
 
 #prune 
 prune_taxa(CPP_only_ASVs_rectal_0.001,CPP_RA) %>%
@@ -54,75 +63,91 @@ prune_taxa(CPP_healthy_ASVs_rectal_0.001,CPP_RA) %>%
   tax_table()
 
 #plot at the genus level - gives the samples within each group that contain the ASVs only present within CPP only and abundance 
-prune_taxa(CPP_only_ASVs_rectal_0.001,CPP_RA) %>% 
+prune_taxa_rectal_CPP_only_0.001 <- prune_taxa(CPP_only_ASVs_rectal_0.001,CPP_RA) %>% 
   plot_bar(fill="Genus") + 
   facet_wrap(.~`Host_disease`, scales ="free")
+
+ggsave("results/aim2/04-core_microbiome/prune_taxa_rectal_CPP_only_0.001.png", prune_taxa_rectal_CPP_only_0.001, width = 7, height = 7)
 
 #plot at the genus level - gives the samples within each group that contain ASVs only present within CPP Endo and abundance 
-prune_taxa(CPP_endo_ASVs_rectal_0.001,CPP_RA) %>% 
+prune_taxa_rectal_CPP_endo_0.001 <- prune_taxa(CPP_endo_ASVs_rectal_0.001,CPP_RA) %>% 
   plot_bar(fill="Genus") + 
   facet_wrap(.~`Host_disease`, scales ="free")
+
+ggsave("results/aim2/04-core_microbiome/prune_taxa_rectal_CPP_endo_0.001.png", prune_taxa_rectal_CPP_endo_0.001, width = 7, height = 7)
 
 ##plot at the genus level - gives the samples within each group that contain ASVs only present within CPP healthy and abundance 
-prune_taxa(CPP_healthy_ASVs_rectal_0.001,CPP_RA) %>% 
+prune_taxa_rectal_CPP_healthy_0.001 <- prune_taxa(CPP_healthy_ASVs_rectal_0.001,CPP_RA) %>% 
   plot_bar(fill="Genus") + 
   facet_wrap(.~`Host_disease`, scales ="free")
 
-#try abundance of 0 - absence/presence 
+ggsave("results/aim2/04-core_microbiome/prune_taxa_rectal_CPP_healthy_0.001.png", prune_taxa_rectal_CPP_healthy_0.001, width = 7, height = 7)
+
+#try detection of 0 (absence/presence) with the 0.5 prevalence  
 CPP_only_ASVs_rectal_0 <- core_members(CPP_only_rectal, detection=0, prevalence = 0.5)
 CPP_endo_ASVs_rectal_0 <- core_members(CPP_endo_rectal, detection=0, prevalence = 0.5)
 CPP_healthy_ASVs_rectal_0 <- core_members(CPP_healthy_rectal, detection=0, prevalence = 0.5)
 
 CPP_list_rectal_0 <- list(Healthy = CPP_healthy_ASVs_rectal_0, CPP_Only = CPP_only_ASVs_rectal_0, CPP_Endo = CPP_endo_ASVs_rectal_0)
 
-#then make venn diagram
 venn_all_diseases_rectal_0<- ggVennDiagram(x = CPP_list_rectal_0)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8") +
   labs(title = "Rectal Samples") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   theme(plot.title = element_text(size=20, face="bold"))
 venn_all_diseases_rectal_0
 
-ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.png", venn_all_diseases_rectal_0)
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.png", venn_all_diseases_rectal_0, width = 7, height = 7)
 
-#try abundance of 0.01 to keep only abundant 
+#try detection of 0.01 with 0.5 prevalence to keep only abundant taxa 
 CPP_only_ASVs_rectal_0.01 <- core_members(CPP_only_rectal, detection=0.01, prevalence = 0.5)
 CPP_endo_ASVs_rectal_0.01 <- core_members(CPP_endo_rectal, detection=0.01, prevalence = 0.5)
 CPP_healthy_ASVs_rectal_0.01 <- core_members(CPP_healthy_rectal, detection=0.01, prevalence = 0.5)
 
 CPP_list_rectal_0.01 <- list(Healthy = CPP_healthy_ASVs_rectal_0.01, CPP_Only = CPP_only_ASVs_rectal_0.01, CPP_Endo = CPP_endo_ASVs_rectal_0.01)
 
-#then make venn diagram
 venn_all_diseases_rectal_0.01<- ggVennDiagram(x = CPP_list_rectal_0.01)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8") +
   labs(title = "Rectal Samples") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   theme(plot.title = element_text(size=20, face="bold"))
 venn_all_diseases_rectal_0.01
 
-ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.01.png", venn_all_diseases_rectal_0.01)
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.01.png", venn_all_diseases_rectal_0.01, width = 7, height = 7)
 
+#try doing an abundance of 0.3 with 0.001 prevalence threshold 
+CPP_only_ASVs_rectal_0.3 <- core_members(CPP_only_rectal, detection=0.001, prevalence = 0.3)
+CPP_endo_ASVs_rectal_0.3 <- core_members(CPP_endo_rectal, detection=0.001, prevalence = 0.3)
+CPP_healthy_ASVs_rectal_0.3 <- core_members(CPP_healthy_rectal, detection=0.001, prevalence = 0.3)
 
-#VAGINAL
-#try stratifying by the vaginal sample instead now  
+CPP_list_rectal_0.3 <- list(Healthy = CPP_healthy_ASVs_rectal_0.3, CPP_Only = CPP_only_ASVs_rectal_0.3, CPP_Endo = CPP_endo_ASVs_rectal_0.3)
+
+venn_all_diseases_rectal_0.3 <- ggVennDiagram(x = CPP_list_rectal_0.3)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
+  labs(title = "Rectal Samples") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(plot.title = element_text(size=20, face="bold"))
+venn_all_diseases_rectal_0.3
+
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_rectal_0.3.png", venn_all_diseases_rectal_0.3, width = 7, height = 7)
+
+#### VAGINAL COREMICROBIOME #### 
+
+#stratifying by the vaginal sample instead now  
 CPP_only_vaginal <- subset_samples(CPP_only,`collection_method` == "vaginal_swab")
 CPP_endo_vaginal <- subset_samples(CPP_endo,`collection_method` == "vaginal_swab")
 CPP_healthy_vaginal <-subset_samples(CPP_healthy, `collection_method` == "vaginal_swab")
 
-#identify thresholds of prevalence/abundance 
 CPP_only_ASVs_vaginal_0.001 <- core_members(CPP_only_vaginal, detection=0.001, prevalence = 0.5)
 CPP_endo_ASVs_vaginal_0.001 <- core_members(CPP_endo_vaginal, detection=0.001, prevalence = 0.5)
 CPP_healthy_ASVs_vaginal_0.001 <- core_members(CPP_healthy_vaginal, detection=0.001, prevalence = 0.5)
 
-#create list 
 CPP_list_vaginal_0.001 <- list(Healthy = CPP_healthy_ASVs_vaginal_0.001, CPP_Only = CPP_only_ASVs_vaginal_0.001, CPP_Endo = CPP_endo_ASVs_vaginal_0.001)
 
-#create a venn diagram 
 venn_all_diseases_vaginal_0.001 <- ggVennDiagram(x = CPP_list_vaginal_0.001)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
   labs(title = "Vaginal Samples") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   theme(plot.title = element_text(size=20, face="bold"))
 venn_all_diseases_vaginal_0.001
 
-ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.001.png", venn_all_diseases_vaginal_0.001)
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.001.png", venn_all_diseases_vaginal_0.001, width = 7, height = 7)
  
 #prune 
 prune_taxa(CPP_only_ASVs_vaginal_0.001,CPP_RA) %>%
@@ -135,50 +160,99 @@ prune_taxa(CPP_healthy_ASVs_vaginal_0.001,CPP_RA) %>%
   tax_table()
 
 #plot at the genus level - gives the samples within each group that contain the ASVs only present within CPP only and abundance 
-prune_taxa(CPP_only_ASVs_vaginal_0.001,CPP_RA) %>% 
+prune_taxa_vaginal_CPP_only_0.001 <- prune_taxa(CPP_only_ASVs_vaginal_0.001,CPP_RA) %>% 
   plot_bar(fill="Genus") + 
   facet_wrap(.~`Host_disease`, scales ="free")
+
+ggsave("results/aim2/04-core_microbiome/prune_taxa_vaginal_CPP_only_0.001.png", prune_taxa_vaginal_CPP_only_0.001, width = 7, height = 7)
 
 #plot at the genus level - gives the samples within each group that contain ASVs only present within CPP Endo and abundance 
-prune_taxa(CPP_endo_ASVs_vaginal_001,CPP_RA) %>% 
+prune_taxa_vaginal_CPP_endo_0.001 <- prune_taxa(CPP_endo_ASVs_vaginal_0.001,CPP_RA) %>% 
   plot_bar(fill="Genus") + 
   facet_wrap(.~`Host_disease`, scales ="free")
+
+ggsave("results/aim2/04-core_microbiome/prune_taxa_vaginal_CPP_endo_0.001.png", prune_taxa_vaginal_CPP_endo_0.001, width = 7, height = 7)
 
 ##plot at the genus level - gives the samples within each group that contain ASVs only present within CPP healthy and abundance 
-prune_taxa(CPP_healthy_ASVs_vaginal_0.001,CPP_RA) %>% 
+prune_taxa_vaginal_CPP_healthy_0.001 <- prune_taxa(CPP_healthy_ASVs_vaginal_0.001,CPP_RA) %>% 
   plot_bar(fill="Genus") + 
   facet_wrap(.~`Host_disease`, scales ="free")
 
-#try abundance of 0 
+ggsave("results/aim2/04-core_microbiome/prune_taxa_vaginal_CPP_healthy_0.001.png", prune_taxa_vaginal_CPP_healthy_0.001, width = 7, height = 7)
+
+#try detection of 0 with 0.5 prevalence
 CPP_only_ASVs_vaginal_0 <- core_members(CPP_only_vaginal, detection=0, prevalence = 0.5)
 CPP_endo_ASVs_vaginal_0 <- core_members(CPP_endo_vaginal, detection=0, prevalence = 0.5)
 CPP_healthy_ASVs_vaginal_0 <- core_members(CPP_healthy_vaginal, detection=0, prevalence = 0.5)
 
-#create list 
 CPP_list_vaginal_0 <- list(Healthy = CPP_healthy_ASVs_vaginal_0, CPP_Only = CPP_only_ASVs_vaginal_0, CPP_Endo = CPP_endo_ASVs_vaginal_0)
 
-#create a venn diagram 
 venn_all_diseases_vaginal_0 <- ggVennDiagram(x = CPP_list_vaginal_0)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
   labs(title = "Vaginal Samples") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   theme(plot.title = element_text(size=20, face="bold"))
 venn_all_diseases_vaginal_0
 
-ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.png", venn_all_diseases_vaginal_0)
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.png", venn_all_diseases_vaginal_0, width = 7, height = 7)
 
-#try abundance of 0.01 
+#get the taxonomic information for CPP_endo disease group and find the ASVs that are unique to the core of CPP-endo 
+tax_mat_vaginal <- tax_table(CPP_endo_vaginal)
+
+core_taxonomy_vaginal <- as.data.frame(tax_mat[CPP_endo_ASVs_vaginal_0 , ])
+print(head(core_taxonomy))
+
+unique_to_CPP_endo_vaginal <- setdiff(CPP_endo_ASVs_vaginal_0, union(CPP_only_ASVs_vaginal_0, CPP_healthy_ASVs_vaginal_0))
+print(unique_to_CPP_endo_vaginal)
+
+#try detection of 0.01 with 0.5 prevalence
 CPP_only_ASVs_vaginal_0.01 <- core_members(CPP_only_vaginal, detection=0.01, prevalence = 0.5)
 CPP_endo_ASVs_vaginal_0.01 <- core_members(CPP_endo_vaginal, detection=0.01, prevalence = 0.5)
 CPP_healthy_ASVs_vaginal_0.01 <- core_members(CPP_healthy_vaginal, detection=0.01, prevalence = 0.5)
 
-#create list 
 CPP_list_vaginal_0.01 <- list(Healthy = CPP_healthy_ASVs_vaginal_0.01, CPP_Only = CPP_only_ASVs_vaginal_0.01, CPP_Endo = CPP_endo_ASVs_vaginal_0.01)
 
-#create a venn diagram 
 venn_all_diseases_vaginal_0.01 <- ggVennDiagram(x = CPP_list_vaginal_0.01)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
   labs(title = "Vaginal Samples") + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   theme(plot.title = element_text(size=20, face="bold"))
 venn_all_diseases_vaginal_0.01
 
-ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.01.png", venn_all_diseases_vaginal_0.01)
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.01.png", venn_all_diseases_vaginal_0.01, width = 7, height = 7)
+
+#try doing an abundnace of 0.3 with detection of 0.001
+CPP_only_ASVs_vaginal_0.3 <- core_members(CPP_only_vaginal, detection=0.001, prevalence = 0.3)
+CPP_endo_ASVs_vaginal_0.3 <- core_members(CPP_endo_vaginal, detection=0.001, prevalence = 0.3)
+CPP_healthy_ASVs_vaginal_0.3 <- core_members(CPP_healthy_vaginal, detection=0.001, prevalence = 0.3)
+
+print(CPP_only_ASVs_vaginal_0.3)
+print (CPP_endo_ASVs_vaginal_0.3)
+print (CPP_healthy_ASVs_vaginal_0.3)
+
+CPP_list_vaginal_0.3 <- list(Healthy = CPP_healthy_ASVs_vaginal_0.3, CPP_Only = CPP_only_ASVs_vaginal_0.3, CPP_Endo = CPP_endo_ASVs_vaginal_0.3)
+
+venn_all_diseases_vaginal_0.3 <- ggVennDiagram(x = CPP_list_vaginal_0.3)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
+  labs(title = "Vaginal Samples") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(plot.title = element_text(size=20, face="bold"))
+venn_all_diseases_vaginal_0.3
+
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_0.3.png", venn_all_diseases_vaginal_0.3, width = 7, height = 7)
+
+
+
+
+#testing a prevalence of 0.2 and a detection of 0.001
+
+CPP_only_ASVs_vaginal_prevalence_0.2 <- core_members(CPP_only_vaginal, detection=0.001, prevalence = 0.2)
+CPP_endo_ASVs_vaginal_prevalence_0.2 <- core_members(CPP_endo_vaginal, detection=0.001, prevalence = 0.2)
+CPP_healthy_ASVs_vaginal_prevalence_0.2 <- core_members(CPP_healthy_vaginal, detection=0.001, prevalence = 0.2)
+
+CPP_list_vaginal_prevalence_0.2 <- list(Healthy = CPP_healthy_ASVs_vaginal_prevalence_0.2, CPP_Only = CPP_only_ASVs_vaginal_prevalence_0.2, CPP_Endo = CPP_endo_ASVs_vaginal_prevalence_0.2)
+
+venn_all_diseases_vaginal_prevalence_0.2 <- ggVennDiagram(x = CPP_list_vaginal_prevalence_0.2)+ scale_fill_gradient(low ="#c7e9b4", high = "#225ea8")+
+  labs(title = "Vaginal Samples") + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  theme(plot.title = element_text(size=20, face="bold"))
+venn_all_diseases_vaginal_prevalence_0.2
+ggsave("results/aim2/04-core_microbiome/venn_all_diseases_vaginal_prevalence_0.2.png", venn_all_diseases_vaginal_prevalence_0.2, width = 7, height = 7)
+
