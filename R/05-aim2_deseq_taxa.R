b872e85336bd017eb01bd8e671065c52c4033a86
brianna-xu <xubrianna2004@gmail.com>
2026-04-04 20:32:34 -0700

    refactor deseq

--- R/05-aim2_deseq_taxa.R
+++ R/05-aim2_deseq_taxa.R
@@ -1,9 +1,10 @@
-
 library(DESeq2)
 library(phyloseq)
 library(EnhancedVolcano)
 library(ggrepel)
 library(patchwork)
+library(dplyr)
+library(tibble)
 
 phylo <- readRDS('data/phyloseq_filtered.rds')
 
@@ -68,7 +69,11 @@ cpp_control_rect <- res_cpp_control_rect %>%
   ) +
   geom_vline(xintercept = 2, linetype = "dashed")+
   geom_vline(xintercept = -2, linetype = "dashed") +
-  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed")
+  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") +
+  labs(
+    x = "Log2 Fold Change",
+    y = "-Log10 (Adjusted P-value)",
+  ) 
 
 cpp_control_vag <- res_cpp_control_vag %>%
   mutate(significant = ifelse(padj<0.05 & abs(log2FoldChange)>2, "Significant", "Non-significant")) %>%
@@ -84,10 +89,11 @@ cpp_control_vag <- res_cpp_control_vag %>%
     ) +
   geom_vline(xintercept = 2, linetype = "dashed")+
   geom_vline(xintercept = -2, linetype = "dashed") +
-  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed")
-
-cpp_control_rect <- cpp_control_rect
-cpp_control_vag  <- cpp_control_vag
+  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") +
+  labs(
+    x = "Log2 Fold Change",
+    y = "-Log10 (Adjusted P-value)",
+  ) 
 
 bar_panel <- cpp_control_rect + cpp_control_vag
 bar_panel
@@ -109,8 +115,8 @@ df_cpp_endo_control_rectal_taxa <- cbind(df_res_cpp_endo_control_rect, taxdf_rec
 df_cpp_endo_control_vaginal_taxa <- cbind(df_res_cpp_endo_control_vag, taxdf_vaginal)
 
 
-write_tsv(df_cpp_endo_control_rectal_taxa, "results/aim2/05-deseq2/05-cpp_endo_control_rect_results.tsv")
-write_tsv(df_cpp_endo_control_vaginal_taxa, "results/aim2/05-deseq2/05-cpp_endo_control_vag_results.tsv")
+# write_tsv(df_cpp_endo_control_rectal_taxa, "results/aim2/05-deseq2/05-cpp_endo_control_rect_results.tsv")
+# write_tsv(df_cpp_endo_control_vaginal_taxa, "results/aim2/05-deseq2/05-cpp_endo_control_vag_results.tsv")
 
 
 ggplot(res_cpp_endo_control_rect) +
@@ -136,7 +142,11 @@ cpp_endo_control_rect <- res_cpp_endo_control_rect %>%
   ) +
   geom_vline(xintercept = 2, linetype = "dashed")+
   geom_vline(xintercept = -2, linetype = "dashed") +
-  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") 
+  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed")+
+  labs(
+    x = "Log2 Fold Change",
+    y = "-Log10 (Adjusted P-value)",
+  ) 
 
 
 cpp_endo_control_vag <- res_cpp_endo_control_vag %>%
@@ -153,10 +163,12 @@ cpp_endo_control_vag <- res_cpp_endo_control_vag %>%
   ) +
   geom_vline(xintercept = 2, linetype = "dashed")+
   geom_vline(xintercept = -2, linetype = "dashed") +
-  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") 
+  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") +
+  labs(
+    x = "Log2 Fold Change",
+    y = "-Log10 (Adjusted P-value)",
+  ) 
 
-cpp_endo_control_rect <- cpp_endo_control_rect
-cpp_endo_control_vag  <- cpp_endo_control_vag
 
 bar_panel_endo_control <- cpp_endo_control_rect + cpp_endo_control_vag
 bar_panel_endo_control
@@ -178,8 +190,8 @@ df_res_rect_taxa <- cbind(df_res_rect, taxdf_rectal)
 df_res_vag_taxa <- cbind(df_res_vag, taxdf_vaginal)
 
 
-write_tsv(df_res_rect_taxa, "results/aim2/05-deseq2/05-cpp_cpp_endo_rect_results.tsv")
-write_tsv(df_res_vag_taxa, "results/aim2/05-deseq2/05-cpp_cpp_endo_vag_results.tsv")
+# write_tsv(df_res_rect_taxa, "results/aim2/05-deseq2/05-cpp_cpp_endo_rect_results.tsv")
+# write_tsv(df_res_vag_taxa, "results/aim2/05-deseq2/05-cpp_cpp_endo_vag_results.tsv")
 
 
 ggplot(res_rect) +
@@ -245,7 +257,11 @@ cpp_endo_cpp_rect <- ggplot(df_res_rect_taxa) +
   ) +
   geom_vline(xintercept = 2, linetype = "dashed")+
   geom_vline(xintercept = -2, linetype = "dashed") +
-  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed")
+  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") +
+  labs(
+    x = "Log2 Fold Change",
+    y = "-Log10 (Adjusted P-value)",
+  ) 
 
 
 # Highlight unique significant genera in CPP-endo/CPP (Vaginal) 
@@ -300,10 +316,12 @@ cpp_endo_cpp_vag <- ggplot(df_res_vag_taxa) +
   ) +
   geom_vline(xintercept = 2, linetype = "dashed")+
   geom_vline(xintercept = -2, linetype = "dashed") +
-  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed")
+  geom_abline(slope = 0, intercept = -log10(0.05),linetype = "dashed") +
+  labs(
+    x = "Log2 Fold Change",
+    y = "-Log10 (Adjusted P-value)",
+  ) 
 
-cpp_endo_cpp_rect <- cpp_endo_cpp_rect
-cpp_endo_cpp_vag  <- cpp_endo_cpp_vag
 
 bar_panel_endo_cpp <- cpp_endo_cpp_rect + cpp_endo_cpp_vag
 bar_panel_endo_cpp
