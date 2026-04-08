ed1222470f3f9c5bbaa233f36a169e2dec300ad2
Tom Cui <tomcui@dhcp-206-87-122-252.ubcsecure.wireless.ubc.ca>
2026-04-08 14:17:20 -0700

    hello

--- R/06-aim3_picrust2_processing.R
+++ R/06-aim3_picrust2_processing.R
@@ -180,6 +180,22 @@ vaginal_KO_PCA <- ggplot(
   )
 rectal_KO_PCA
 vaginal_KO_PCA
+
+combined_KO_PCA <- rectal_KO_PCA + vaginal_KO_PCA
+combined_KO_PCA
+
+ggsave(
+  "results/aim3/KO_rectal_vaginal_PCA_combined.png",
+  plot = combined_KO_PCA,
+  width = 16, height = 7, units = "in", dpi = 300
+)
+
+ggsave(
+  "results/aim3/KO_rectal_vaginal_PCA_combined.png",
+  plot = combined_KO_PCA,
+  width = 16, height = 7, units = "in", dpi = 300
+)
+
 ggsave("results/aim3/KO_rectal_pcoa_ellipse.png",
        plot = rectal_KO_PCA,
        width = 10, height = 7, units = "in", dpi = 300)
@@ -667,3 +683,39 @@ ggsave(
   width = 10, height = 7, units = "in", dpi = 300
 )
 
+##########Bar###############
+df <- data.frame(
+  pathway = c(
+    "Metabolic pathways",
+    "ABC transporters",
+    "Lipopolysaccharide biosynthesis",
+    "Biosynthesis of unsaturated fatty acids",
+    "Cationic antimicrobial peptide (CAMP) resistance",
+    "Bacterial secretion system",
+    "Biosynthesis of cofactors",
+    "Thiamine metabolism",
+    "Protein export"
+  ),
+  count = c(2, 2, 1, 1, 1, 1, 1, 1, 1)
+)
+
+df <- df %>% arrange(count)
+
+bar <- ggplot(df, aes(x = count, y = reorder(pathway, count), fill = count)) +
+  geom_col() +
+  geom_text(aes(label = count), hjust = -0.15, size = 4) +
+  scale_fill_gradient(low = "#1B98E026", high="#225ea8") +
+  labs(
+    x = "Number of associated enriched KOs",
+    y = NULL,
+    fill = "Count"
+  ) +
+  theme_classic(base_size = 12) +
+  xlim(0, max(df$count) + 0.5)
+
+ggsave(
+  "results/aim3/bar.png",
+  plot = bar,
+  width = 10, height = 5, units = "in", dpi = 300
+)
+bar
